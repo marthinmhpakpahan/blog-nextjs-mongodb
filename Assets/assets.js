802885f1e17515c93ad0e55e3c4f242d798cb78d
Marthin Pakpahan <marthinpakpahan@Marthins-MacBook-Pro.local>
2024-12-17 15:43:39 +0700

    initial commit and add header section

---
+++ Assets/assets.js
@@ -0,0 +1,42 @@
+import blog_pic_1 from './blog_pic_1.jpg';
+import blog_pic_2 from './blog_pic_2.jpg';
+import blog_pic_3 from './blog_pic_3.jpg';
+import blog_pic_4 from './blog_pic_4.jpg';
+import blog_pic_5 from './blog_pic_5.jpg';
+import blog_pic_6 from './blog_pic_6.jpg';
+import blog_pic_7 from './blog_pic_7.jpg';
+import blog_pic_8 from './blog_pic_8.jpg';
+import blog_pic_9 from './blog_pic_9.jpg';
+import blog_pic_10 from './blog_pic_10.jpg';
+import blog_pic_11 from './blog_pic_11.jpg';
+import blog_pic_12 from './blog_pic_12.jpg';
+import blog_pic_13 from './blog_pic_13.jpg';
+import blog_pic_14 from './blog_pic_14.jpg';
+import blog_pic_15 from './blog_pic_15.jpg';
+import blog_pic_16 from './blog_pic_16.jpg';
+import facebook_icon from './facebook_icon.png';
+import googleplus_icon from './googleplus_icon.png';
+import twitter_icon from './twitter_icon.png';
+import profile_icon from './profile_icon.png';
+import logo from './logo.png';
+import arrow from './arrow.png';
+import logo_light from './logo_light.png';
+import blog_icon from './blog_icon.png';
+import add_icon from './add_icon.png';
+import email_icon from './email_icon.png';
+import upload_area from './upload_area.png';
+
+export const assets = {
+    facebook_icon,
+    googleplus_icon,
+    twitter_icon,
+    twitter_icon,
+    profile_icon,
+    logo,
+    arrow,
+    logo_light,
+    blog_icon,
+    add_icon,
+    email_icon,
+    upload_area
+};
---
+++ Components/Header.jsx
@@ -0,0 +1,22 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className="py-5 px-5 md:px-12 lg:px-28">
+        <div className="flex justify-between items-center">
+            <Image src={assets.logo} width={50} alt="" className="w-[130px] sm:w-auto"/>
+            <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]">Get started <Image src={assets.arrow} width={20} alt=""/></button>
+        </div>
+        <div className="text-center my-8">
+            <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
+            <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sed dolor viverra libero luctus tempor. Pellentesque et gravida magna. Curabitur nec quam ex. In hac habitasse platea dictumst. Etiam non purus dui. Aliquam porttitor purus id accumsan cursus. Maecenas ac nisi nec leo rhoncus ornare.</p>
+            <form className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]" action="">
+                <input className="pl-4 outline-none" type="email" placeholder="Enter your email"/>
+                <button type="submit" className="border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white">Subscribe</button>
+            </form>
+        </div>
+    </div>
+  )
+}
