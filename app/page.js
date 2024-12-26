86c5e8335b4624e295f2aec6f23e881275b5daf4
Marthin Pakpahan <marthinpakpahan@Marthins-MacBook-Pro.local>
2024-12-26 12:49:32 +0700

    add blog list component

--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
 'use client'
 
-import { BlogItem } from "@/Components/BlogItem";
+import { BlogList } from "@/Components/BlogList";
 import { Header } from "@/Components/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Header/>
-      <BlogItem/>
+      <BlogList/>
     </>
   );
 }
