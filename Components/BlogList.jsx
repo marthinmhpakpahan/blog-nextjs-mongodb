a227031bcd3b1cf443554af6f3ff8dfe0f947434
Marthin Pakpahan <marthinpakpahan@Marthins-MacBook-Pro.local>
2025-01-31 15:30:01 +0700

    update static data and filter data by menu category

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -1,23 +1,26 @@
 import { blog_data } from '@/Assets/assets'
 import React from 'react'
 import { BlogItem } from './BlogItem'
+import { useState } from 'react'
 
 export const BlogList = () => {
-  return (
-    <div>
-        <div className="flex justify-center gap-6 my-10">
-            <button className="bg-black text-white py-1 px-4 rounded-sm">All</button>
-            <button>Technology</button>
-            <button>Startup</button>
-            <button>Lifestyle</button>
+
+    const [menu, setMenu] = useState("All");
+    return (
+        <div>
+            <div className="flex justify-center gap-6 my-10">
+                <button onClick={() => setMenu('All')} className={menu === "All" ? "bg-black text-white py-1 px-4 rounded-sm" : ""}>All</button>
+                <button onClick={() => setMenu('Technology')} className={menu === "Technology" ? "bg-black text-white py-1 px-4 rounded-sm" : ""}>Technology</button>
+                <button onClick={() => setMenu('Startup')} className={menu === "Startup" ? "bg-black text-white py-1 px-4 rounded-sm" : ""}>Startup</button>
+                <button onClick={() => setMenu('Lifestyle')} className={menu === "Lifestyle" ? "bg-black text-white py-1 px-4 rounded-sm" : ""}>Lifestyle</button>
+            </div>
+            <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
+                {
+                    blog_data.filter((item) => menu === "All" ? true : item.category === menu).map((item) => {
+                        return <BlogItem key={item.id} title={item.title} description={item.description} category={item.category} image={item.image} />
+                    })
+                }
+            </div>
         </div>
-        <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
-            {
-                blog_data.map((item) => {
-                    return <BlogItem key={item.id} title={item.title} description={item.description} category={item.category} image={item.image} />
-                })
-            }
-        </div>
-    </div>
-  )
+    )
 }
