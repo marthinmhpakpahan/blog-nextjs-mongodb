44f13171f9a6c051e0335303cad4df20c503651d
Marthin Pakpahan <marthinpakpahan@Marthins-MacBook-Pro.local>
2025-02-03 11:01:50 +0700

    add footer and change some icons

---
+++ Components/Footer.jsx
@@ -0,0 +1,19 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="flex justify-around flex-col gap-2 sm:gap-0 sm:flex-row bg-black py-5 items-center">
+        <Image src={assets.logo_light} alt='' width={40} />
+        <p className='text-sm text-white'>All right reserved. Copyright @mmhp.tech</p>
+        <div className='flex'>
+            <Image className="mx-1" src={assets.facebook_icon} alt='' width={40} />
+            <Image className="mx-1" src={assets.twitter_icon} alt='' width={40} />
+            <Image className="mx-1" src={assets.googleplus_icon} alt='' width={40} />
+        </div>
+    </div>
+  )
+}
+
+export default Footer
